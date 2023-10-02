eb1d0cce847fc4df840419a5f89cce5dba8381b5
Haveadream1 <260602david@gmail.com>
2023-10-02 08:08:36 +0200

    Responsive design

--- src/home.js
+++ src/home.js
@@ -2,4 +2,16 @@ const home = () => {
 
 };
 
+/*  Beige section :
+        Next to Seoul put a button to change location
+        Depend on the location value, take data and display it
+
+    Red section :
+        Display the image that depend on the actual weather
+
+    Purple section :
+        Display the weather for the week
+        Day + t*
+*/
+
 export default home;
