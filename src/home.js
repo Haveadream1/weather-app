74ddffca6f6f4f3b3016ca310367c06355b18fc4
Haveadream1 <260602david@gmail.com>
2023-10-06 11:37:05 +0200

    Work fetch

--- src/home.js
+++ src/home.js
@@ -1,22 +1,19 @@
 const home = () => {
-  // issue console
-  const cityChoice = document.querySelector('.city-choice');
-  console.log('dedw')
+  const cityText = document.querySelector('.city-choice');
+  const cityChoice = 'Seoul';
+  const apiKey = 'bce6611d55994183931152601230107';
+  const apiUrl = 'https://api.weatherapi.com/v1/forecast.json?';
+  // const KEY = 'https://api.weatherapi.com/v1/forecast.json?key=bce6611d55994183931152601230107&q=Seoul&days=1&aqi=no&alerts=no'
 
   async function getWeather() {
-    const response = await fetch('https://api.weatherapi.com/v1/forecast.json?key=bce6611d55994183931152601230107&q=Seoul&days=1&aqi=no&alerts=no', { mode: 'cors' });
+    const response = await fetch(`${apiUrl}key=${apiKey}&q=${cityChoice}&days=1&aqi=no&alerts=no`, { mode: 'cors' });
     const data = await response.json();
     console.log(data);
-    // cityChoice.textContent = title.location.name.value();
+    cityText.textContent = data.location.name;
   }
   getWeather();
 };
-/*
-  async function getCats() {
-    const response = await fetch('https://api.giphy.com/v1/gifs/translate?api_key=YOUR_KEY_HERE&s=cats', {mode: 'cors'});
-    const catData = await response.json();
-    img.src = catData.data.images.original.url;
-  }
+export default home;
 
 /*  Beige section :
         Next to Seoul put a button to change location
@@ -29,5 +26,3 @@ const home = () => {
         Display the weather for the week
         Day + t*
 */
-
-export default home;
