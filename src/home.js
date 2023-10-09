6e176e5a68f0289ecf61c93cb3ad3e9a81633905
Haveadream1 <260602david@gmail.com>
2023-10-09 09:29:31 +0200

    Change main cont, fetch all data

--- src/home.js
+++ src/home.js
@@ -1,7 +1,17 @@
 const home = () => {
-  const cityText = document.querySelector('.city-text');
+  // eslint-disable prefer-destructuring
   const cityInputEl = document.querySelector('#city-input');
   const form = document.querySelector('#form');
+
+  const mainText = document.querySelector('.main-text');
+  const mainDate = document.querySelector('.main-date');
+  const mainTemp = document.querySelector('.main-temp');
+  const mainImg = document.querySelector('.main-img');
+
+  const childSection1 = document.querySelector('.child-section1');
+  const childSection2 = document.querySelector('.child-section2');
+  const childSection3 = document.querySelector('.child-section3');
+  const childSection4 = document.querySelector('.child-section4');
 
   const apiKey = 'bce6611d55994183931152601230107';
   const apiUrl = 'https://api.weatherapi.com/v1/forecast.json?';
@@ -9,10 +19,38 @@ const home = () => {
 
   async function getWeather() {
     const cityChoice = cityInputEl.value.trim();
-    const response = await fetch(`${apiUrl}key=${apiKey}&q=${cityChoice}&days=1&aqi=no&alerts=no`, { mode: 'cors' });
+    const response = await fetch(`${apiUrl}key=${apiKey}&q=${cityChoice}&days=8&aqi=no&alerts=no`, { mode: 'cors' });
     const data = await response.json();
     console.log(data);
-    cityText.textContent = data.location.name;
+
+    mainText.textContent = data.location.name;
+    const formatDate = data.current.last_updated;
+    mainDate.textContent = formatDate.slice(0, 11); // format date
+    // call dateAPI
+    mainTemp.textContent = `${data.current.temp_c}°c`;
+
+    mainImg.src = data.current.condition.icon;
+
+    // time section data
+    const childSection1Path = data.forecast.forecastday[0].hour[6];
+    const childSection1Date = childSection1Path.time;
+    childSection1.querySelector('.time').textContent = childSection1Date.slice(11, 16);
+    childSection1.querySelector('.temp').textContent = `${childSection1Path.temp_c}°c`;
+
+    const childSection2Path = data.forecast.forecastday[0].hour[12];
+    const childSection2Date = childSection2Path.time;
+    childSection2.querySelector('.time').textContent = childSection2Date.slice(11, 16);
+    childSection2.querySelector('.temp').textContent = `${childSection2Path.temp_c}°c`;
+
+    const childSection3Path = data.forecast.forecastday[0].hour[16];
+    const childSection3Date = childSection3Path.time;
+    childSection3.querySelector('.time').textContent = childSection3Date.slice(11, 16);
+    childSection3.querySelector('.temp').textContent = `${childSection3Path.temp_c}°c`;
+
+    const childSection4Path = data.forecast.forecastday[0].hour[21];
+    const childSection4Date = childSection4Path.time;
+    childSection4.querySelector('.time').textContent = childSection4Date.slice(11, 16);
+    childSection4.querySelector('.temp').textContent = `${childSection4Path.temp_c}°c`;
   }
 
   const isRequired = (value) => {
