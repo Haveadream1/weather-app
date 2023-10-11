32ea5e8fd5cabe71ea2344befdfe975d5ed15ad8
Haveadream1 <260602david@gmail.com>
2023-10-11 08:45:14 +0200

    Main img, alt, fix img today sect

--- src/home.js
+++ src/home.js
@@ -1,5 +1,5 @@
+// eslint-disable prefer-destructuring
 const home = () => {
-  // eslint-disable prefer-destructuring
   const cityInputEl = document.querySelector('#city-input');
   const form = document.querySelector('#form');
 
@@ -30,30 +30,35 @@ const home = () => {
     mainTemp.textContent = `${data.current.temp_c}°c`;
 
     mainImg.src = data.current.condition.icon;
+    mainImg.alt = data.current.condition.text;
 
     // time section data
     const childSection1Path = data.forecast.forecastday[0].hour[6];
     const childSection1Date = childSection1Path.time;
     childSection1.querySelector('.time').textContent = childSection1Date.slice(11, 16);
     childSection1.querySelector('.img').src = childSection1Path.condition.icon;
+    childSection1.querySelector('.img').alt = childSection1Path.condition.text;
     childSection1.querySelector('.temp').textContent = `${childSection1Path.temp_c}°c`;
 
     const childSection2Path = data.forecast.forecastday[0].hour[12];
     const childSection2Date = childSection2Path.time;
     childSection2.querySelector('.time').textContent = childSection2Date.slice(11, 16);
     childSection2.querySelector('.img').src = childSection2Path.condition.icon;
+    childSection2.querySelector('.img').alt = childSection2Path.condition.text;
     childSection2.querySelector('.temp').textContent = `${childSection2Path.temp_c}°c`;
 
     const childSection3Path = data.forecast.forecastday[0].hour[16];
     const childSection3Date = childSection3Path.time;
     childSection3.querySelector('.time').textContent = childSection3Date.slice(11, 16);
     childSection3.querySelector('.img').src = childSection3Path.condition.icon;
+    childSection3.querySelector('.img').alt = childSection3Path.condition.text;
     childSection3.querySelector('.temp').textContent = `${childSection3Path.temp_c}°c`;
 
     const childSection4Path = data.forecast.forecastday[0].hour[21];
     const childSection4Date = childSection4Path.time;
     childSection4.querySelector('.time').textContent = childSection4Date.slice(11, 16);
     childSection4.querySelector('.img').src = childSection4Path.condition.icon;
+    childSection4.querySelector('.img').alt = childSection4Path.condition.text;
     childSection4.querySelector('.temp').textContent = `${childSection4Path.temp_c}°c`;
   }
 
