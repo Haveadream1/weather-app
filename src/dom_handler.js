e476bb8228c6203f277f5521ccd9a76abcbd745c
Haveadream1 <260602david@gmail.com>
2025-03-23 14:31:20 +0100

    refactor: variables naming

--- src/dom_handler.js
+++ src/dom_handler.js
@@ -26,43 +26,43 @@ export const displayCurrentDate = () => {
 
 export const displayFooter = (data) => {
   // NEED TO FIND A BETTER WAY => getWeather
-  const childSection1 = document.querySelector('.time-section-1');
-  const childSection2 = document.querySelector('.time-section-2');
-  const childSection3 = document.querySelector('.time-section-3');
-  const childSection4 = document.querySelector('.time-section-4');
+  const timeSection1 = document.querySelector('.time-section-1');
+  const timeSection2 = document.querySelector('.time-section-2');
+  const timeSection3 = document.querySelector('.time-section-3');
+  const timeSection4 = document.querySelector('.time-section-4');
 
   // time section data
-  const childSection1Path = data.forecast.forecastday[0].hour[6];
-  const childSection1Date = childSection1Path.time;
-  childSection1.querySelector('.time').textContent = childSection1Date.slice(11, 16);
+  const timeSection1Path = data.forecast.forecastday[0].hour[6];
+  const timeSection1Date = timeSection1Path.time;
+  timeSection1.querySelector('.time').textContent = timeSection1Date.slice(11, 16);
   // childSection1.querySelector('.img').src = childSection1Path.condition.icon;
-  if (childSection1Path.condition.text === 'Partly Cloudy ') {
-    childSection1.querySelector('.img').src = '/dist/images/Sun.svg';
+  if (timeSection1Path.condition.text === 'Partly Cloudy ') {
+    timeSection1.querySelector('.img').src = '/dist/images/Sun.svg';
   }
 
-  childSection1.querySelector('.img').alt = childSection1Path.condition.text;
-  childSection1.querySelector('.temp').textContent = `${childSection1Path.temp_c}°c`;
+  timeSection1.querySelector('.img').alt = timeSection1Path.condition.text;
+  timeSection1.querySelector('.temp').textContent = `${timeSection1Path.temp_c}°c`;
 
-  const childSection2Path = data.forecast.forecastday[0].hour[12];
-  const childSection2Date = childSection2Path.time;
-  childSection2.querySelector('.time').textContent = childSection2Date.slice(11, 16);
-  childSection2.querySelector('.img').src = childSection2Path.condition.icon;
-  console.log(childSection2Path.condition.icon);
-  childSection2.querySelector('.img').alt = childSection2Path.condition.text;
-  childSection2.querySelector('.temp').textContent = `${childSection2Path.temp_c}°c`;
+  const timeSection2Path = data.forecast.forecastday[0].hour[12];
+  const timeSection2Date = timeSection2Path.time;
+  timeSection2.querySelector('.time').textContent = timeSection2Date.slice(11, 16);
+  timeSection2.querySelector('.img').src = timeSection2Path.condition.icon;
+  console.log(timeSection2Path.condition.icon);
+  timeSection2.querySelector('.img').alt = timeSection2Path.condition.text;
+  timeSection2.querySelector('.temp').textContent = `${timeSection2Path.temp_c}°c`;
 
-  const childSection3Path = data.forecast.forecastday[0].hour[16];
-  const childSection3Date = childSection3Path.time;
-  childSection3.querySelector('.time').textContent = childSection3Date.slice(11, 16);
-  childSection3.querySelector('.img').src = childSection3Path.condition.icon;
-  console.log(childSection3Path.condition.icon);
-  childSection3.querySelector('.img').alt = childSection3Path.condition.text;
-  childSection3.querySelector('.temp').textContent = `${childSection3Path.temp_c}°c`;
+  const timeSection3Path = data.forecast.forecastday[0].hour[16];
+  const timeSection3Date = timeSection3Path.time;
+  timeSection3.querySelector('.time').textContent = timeSection3Date.slice(11, 16);
+  timeSection3.querySelector('.img').src = timeSection3Path.condition.icon;
+  console.log(timeSection3Path.condition.icon);
+  timeSection3.querySelector('.img').alt = timeSection3Path.condition.text;
+  timeSection3.querySelector('.temp').textContent = `${timeSection3Path.temp_c}°c`;
 
-  const childSection4Path = data.forecast.forecastday[0].hour[21];
-  const childSection4Date = childSection4Path.time;
-  childSection4.querySelector('.time').textContent = childSection4Date.slice(11, 16);
-  childSection4.querySelector('.img').src = childSection4Path.condition.icon;
-  childSection4.querySelector('.img').alt = childSection4Path.condition.text;
-  childSection4.querySelector('.temp').textContent = `${childSection4Path.temp_c}°c`;
+  const timeSection4Path = data.forecast.forecastday[0].hour[21];
+  const timeSection4Date = timeSection4Path.time;
+  timeSection4.querySelector('.time').textContent = timeSection4Date.slice(11, 16);
+  timeSection4.querySelector('.img').src = timeSection4Path.condition.icon;
+  timeSection4.querySelector('.img').alt = timeSection4Path.condition.text;
+  timeSection4.querySelector('.temp').textContent = `${timeSection4Path.temp_c}°c`;
 };
