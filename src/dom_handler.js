722a8db2c21c5cd76ebfcac0ed5d714e3029aaa7
Haveadream1 <260602david@gmail.com>
2025-03-24 13:05:17 +0100

    refactor: update weather conditions

--- src/dom_handler.js
+++ src/dom_handler.js
@@ -7,15 +7,17 @@ import cloudImage from '../dist/images/Cloud.svg';
 import overcastImage from '../dist/images/Overcast.svg';
 
 function getWeatherImage(condition) {
-  console.log(condition);
-  if (condition === 'Cloudy') {
+  const conditionText = condition.text;
+  if (conditionText === 'Cloudy' || conditionText === 'Mist') {
     return cloudImage;
-  } else if (condition === 'Partly Cloudy ' || condition === 'Overcast ') {
+  } else if (conditionText === 'Partly Cloudy ' || conditionText === 'Overcast ' || conditionText === 'Partly Cloudy') {
     return overcastImage;
-  } else if (condition === 'Sunny' || condition === 'Clear ') {
+  } else if (conditionText === 'Sunny' || conditionText === 'Clear ' || conditionText === 'Clear') {
     return sunImage;
-  } else if (condition === 'Rain') {
+  } else if (conditionText === 'Rain' || conditionText === 'Patchy rain nearby') {
     return rainImage;
+  } else {
+    return condition.icon; // Handle other conditions with api icon
   }
 }
 
@@ -31,7 +33,7 @@ export const displayMain = (data) => {
   mainCity.textContent = data.location.name;
   mainTemp.textContent = `${data.current.temp_c}°c`;
 
-  const mainImagePath = getWeatherImage(data.current.condition.text);
+  const mainImagePath = getWeatherImage(data.current.condition);
   mainImg.src = mainImagePath;
   mainImg.alt = data.current.condition.text;
 };
@@ -44,8 +46,6 @@ export const displayCurrentDate = () => {
   mainDate.textContent = formatedDate;
 };
 
-// const conditions = ['Sunny', 'Clear ', 'Partly Cloudy '];
-
 export const displayFooter = (data) => {
   const timeSection1 = document.querySelector('.time-section-1');
   const timeSection2 = document.querySelector('.time-section-2');
@@ -55,7 +55,7 @@ export const displayFooter = (data) => {
   // 1st Cell
   const timeSection1Path = data.forecast.forecastday[0].hour[6];
   const timeSection1Date = timeSection1Path.time;
-  const imagePath1 = getWeatherImage(timeSection1Path.condition.text);
+  const imagePath1 = getWeatherImage(timeSection1Path.condition);
 
   timeSection1.querySelector('.img').src = imagePath1;
   timeSection1.querySelector('.img').alt = timeSection1Path.condition.text;
@@ -65,7 +65,7 @@ export const displayFooter = (data) => {
   // 2nd Cell
   const timeSection2Path = data.forecast.forecastday[0].hour[12];
   const timeSection2Date = timeSection2Path.time;
-  const imagePath2 = getWeatherImage(timeSection2Path.condition.text);
+  const imagePath2 = getWeatherImage(timeSection2Path.condition);
 
   timeSection2.querySelector('.img').src = imagePath2;
   timeSection2.querySelector('.img').alt = timeSection2Path.condition.text;
@@ -75,7 +75,7 @@ export const displayFooter = (data) => {
   // 3rd Cell
   const timeSection3Path = data.forecast.forecastday[0].hour[16];
   const timeSection3Date = timeSection3Path.time;
-  const imagePath3 = getWeatherImage(timeSection3Path.condition.text);
+  const imagePath3 = getWeatherImage(timeSection3Path.condition);
 
   timeSection3.querySelector('.img').src = imagePath3;
   timeSection3.querySelector('.img').alt = timeSection3Path.condition.text;
@@ -85,7 +85,7 @@ export const displayFooter = (data) => {
   // 4th Cell
   const timeSection4Path = data.forecast.forecastday[0].hour[21];
   const timeSection4Date = timeSection4Path.time;
-  const imagePath4 = getWeatherImage(timeSection4Path.condition.text);
+  const imagePath4 = getWeatherImage(timeSection4Path.condition);
 
   timeSection4.querySelector('.img').src = imagePath4;
   timeSection4.querySelector('.img').alt = timeSection4Path.condition.text;
