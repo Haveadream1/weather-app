57add749925982a7b05714a4f3d6761396e181be
Haveadream1 <260602david@gmail.com>
2026-09-18 09:38:47 +0200

    feat: add service worker

--- src/index.js
+++ src/index.js
@@ -2,3 +2,12 @@ import "./style.css";
 import initializeWebsite from "./home";
 
 initializeWebsite();
+
+// Service worker registration
+// Middleware between the PWA and the servers it interacts
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+        navigator.serviceWorker.register("/service-worker.js")
+            .catch((err) => console.error("SW registration failed:", err));
+    })
+}
