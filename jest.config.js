aa60e06c93afc98dd4828a6e56970c4abb77181b
Haveadream1 <260602david@gmail.com>
2026-09-20 14:52:57 +0200

    fix: configure jest to ignore playwright e2e tests directory

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,12 @@
 module.exports = {
+  // Tell Jest to use Babel for modern JS syntax
+  transform: {
+    "^.+\\.js$": "babel-jest",
+  },
+  // Tell Jest to mock these file extensions
   moduleNameMapper: {
     "\\.svg$": "<rootDir>/__mocks__/svgMock.js",
   },
+  // ell Jest to IGNORE the Playwright E2E tests directory
+  testPathIgnorePatterns: ["<rootDir>/tests/"],
 };
